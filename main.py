95824806b4fb17c9b94eda372f71c1debe4c93b9
Bruce Paulson <bpaulson@students.avc.edu>
2025-04-10 20:22:09 -0700

    modified program to get user input

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ def main():
     ########################################
     """
     total = 0
+    count = 0
 
-    i = 0
-    while i < 10:
-        i += 1
-        total += i
-        print("Current value of i:", i)
+    while count < 5:
+        user_input = int(input(f"Enter integer #{count + 1}: "))
+        total += user_input
+        count += 1
 
     ########################################
     # Do not delete the return statement
@@ -19,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    print(main())  # Print the total so user can see the result
